3fc20c379cc6e10b1b82395dd0fb082b59d4a596
G Roques <groques360@gmail.com>
2020-09-07 08:18:42 -0500

    Add initial support for keyword cards

--- tests/parse_inp_test.py
+++ tests/parse_inp_test.py
@@ -62,6 +62,54 @@ class ParseInpTest(unittest.TestCase):
         self.assertEqual(tree.children[1].data, 'single_line_comment')
         self.assertEqual(len(tree.children[1].children), 0)
 
+    def test_parse_inp_with_single_keyword_line_without_params_or_data(self):
+        contents = '*STEP'
+
+        tree = parse_inp(contents)
+
+        self.assertEqual(tree.data, 'start')
+        self.assertEqual(len(tree.children), 1)
+
+        keyword_card = tree.children[0]
+        self.assertEqual(keyword_card.data, 'keyword_card')
+        self.assertEqual(len(keyword_card.children), 1)
+
+        keyword_token = keyword_card.children[0]
+        self.assertEqual(keyword_token.type, 'KEYWORD')
+        self.assertEqual(keyword_token.value, '*STEP')
+
+    def test_parse_inp_with_single_keyword_line_with_params_and_no_data(self):
+        contents = '*INCLUDE,INPUT=/home/guido/test/beam.spc'
+
+        tree = parse_inp(contents)
+
+        self.assertEqual(tree.data, 'start')
+        self.assertEqual(len(tree.children), 1)
+
+        keyword_card = tree.children[0]
+        self.assertEqual(keyword_card.data, 'keyword_card')
+        self.assertEqual(len(keyword_card.children), 2)
+
+        keyword_token = keyword_card.children[0]
+        self.assertEqual(keyword_token.type, 'KEYWORD')
+        self.assertEqual(keyword_token.value, '*INCLUDE')
+
+        param = keyword_card.children[1]
+        self.assertEqual(param.data, 'param')
+        self.assertEqual(len(param.children), 2)
+
+        key = param.children[0]
+        self.assertEqual(key.type, 'KEY')
+        self.assertEqual(key.value, 'INPUT')
+
+        value = param.children[1]
+        self.assertEqual(value.data, 'value')
+        self.assertEqual(len(value.children), 1)
+
+        value_token = value.children[0]
+        self.assertEqual(value_token.type, 'PATH')
+        self.assertEqual(value_token.value, '/home/guido/test/beam.spc')
+
 
 if __name__ == '__main__':
     unittest.main()
